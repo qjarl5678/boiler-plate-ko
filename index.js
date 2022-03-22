ce32e766228e96137194b4dcb5416385ff77be3a
qjarl5678 <qjarl5678@nate.com>
2022-03-22 12:39:53 +0900

    jwt token을 이용한 pw 검증 추가

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 5000;
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config =  require('./config/key.js');
-
 const User = require('./models/User.js');
 
 //application/x-www-form-urlencoded
@@ -13,14 +13,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 const mongoose = require('mongoose');
-
 mongoose.connect(config.mongoURI)
 .then(()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req,res) =>{ res.send('안녕하세요~~ 적당히 바람이 시원해 기분이 너무 좋아요 유후~ goodDay')});
+
 app.post('/register', (req,res) => {
 
     //회원 가입 할 때 필요한 정보들을 client에서 가져오면
@@ -32,6 +34,36 @@ app.post('/register', (req,res) => {
             success: true
         })
     })
+})
+
+app.post('/login', (req, res) => {
+
+    //요청된 이메일을 데이터베이스에서 있는지 확인.
+    User.findOne({ email: req.body.email }, (err, userInfo) => {
+        if(!userInfo){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+            return res.json( {loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+            //비밀번호가 맞다면 토큰을 생성한다.
+            userInfo.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                
+                // 토큰을 저장한다. 어디에 ? 큐키, 로컬스토리지 등등
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+
 })
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
