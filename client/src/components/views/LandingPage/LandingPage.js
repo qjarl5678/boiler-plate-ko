fae44edf8932ef554280ac2368fc8ac69d4956f5
qjarl5678 <qjarl5678@nate.com>
2022-03-29 15:30:56 +0900

    로그아웃 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,27 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 
 function LandingPage() {
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('/api/hello')
     .then(response => console.log(response.data))
   }, [])
+
+  const onClickHandler = () =>{
+    axios.get('/api/users/logout')
+    .then(response => {
+      if(response.data.success){
+        navigate('/login')
+      }else{
+        alert('logout fail...ERR!ERR!ERR!ERR!')
+      }
+      // console.log(response.data);
+    })
+  }
 
 
   return (
@@ -15,6 +30,10 @@ function LandingPage() {
       width: '100%', height: '100vh'
     }}>
       <h2>시작페이지</h2>
+
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
       </div>
   )
 }
